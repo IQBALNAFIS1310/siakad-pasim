f530f00baefaa4146452c142a80e42ded670726a
IQBALNAFIS1310 <iqbalnafis487@gmail.com>
2025-07-25 15:42:12 +0700

    first commit

---
+++ Controllers/LoginPagesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace siakad_pasim.Controllers
+{
+    public class LoginPagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace siakad_pasim.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
